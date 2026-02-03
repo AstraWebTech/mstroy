36cd836fcbfc6db74e81c54a70403b1fb1b46aa5
Гаврилов Евгений Владиславович <rol225rf@mail.ru>
2026-02-03 10:54:31 +0700

    feat(agGridVue): добавлена таблица enterprise

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import { ModuleRegistry } from 'ag-grid-community';
+import { AllEnterpriseModule } from 'ag-grid-enterprise';
+
+ModuleRegistry.registerModules([AllEnterpriseModule]);
+
 createApp(App).mount('#app')
